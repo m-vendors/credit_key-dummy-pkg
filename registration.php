41c447f288286591890a4de80d5b0b33c64eac61
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:00:40 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'credit_key_DummyPkg',
+    __DIR__
+);
